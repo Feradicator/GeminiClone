7e7b7232cb37d54cfec247de6854415c67d1ab47
Feradicator <yadavshivam042001@gmail.com>
2024-05-17 15:08:21 +0530

    added features to side abr

--- myapp/src/components/sidebar/SideBar.jsx
+++ myapp/src/components/sidebar/SideBar.jsx
@@ -10,9 +10,30 @@ const SideBar = () => {
         <img src={assets.plus_icon}/>
         <p>new chat</p>
         </div>
+        <div className="recent">
+            <p className='recent-title'>
+                recent 
+            </p>
+            <div className="recent-entry">
+                <img src={assets.message_icon }/>
+                <p>what is react...</p>
+            </div>
+
+        </div>
         <div className='bottom'>
-
-
+            <div className="bottom-item recent-entry"></div>
+            <img src={assets.question_icon}/>
+            <p>help</p>
+        </div>
+        <div className='bottom'>
+            <div className="bottom-item recent-entry"></div>
+            <img src={assets.history_icon}/>
+            <p>Activity</p>
+        </div>
+        <div className='bottom'>
+            <div className="bottom-item recent-entry"></div>
+            <img src={assets.setting_icon}/>
+            <p>Settings</p>
         </div>
       
     </div>
