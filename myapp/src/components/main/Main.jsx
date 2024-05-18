9f95d399cce17324379f4b63548260beb2b5b5bd
Feradicator <yadavshivam042001@gmail.com>
2024-05-19 00:53:07 +0530

    added card

--- myapp/src/components/main/Main.jsx
+++ myapp/src/components/main/Main.jsx
@@ -8,6 +8,32 @@ const Main = () => {
             <p>Gemini</p>
             <img src={assets.user_icon}></img>
         </div>
+        <div className="main-container">
+            <div className="greet">
+                <p><span>Hello Shivam</span></p>
+                <p>How can I help you today?</p>
+            </div>
+            <div className="cards">
+                <div className="card">
+                    <p>Suggest beautiful place to see on an upcoming road trip</p>
+                    <img src={assets.compass_icon}/>
+                </div>
+                <div className="card">
+                    <p>Briefly summarize this concept : urban planning</p>
+                    <img src={assets.bulb_icon}/>
+                </div>
+                <div className="card">
+                    <p>Brainstorm team bonding activities for work retreat</p>
+                    <img src={assets.message_icon}/>
+                </div>
+                <div className="card">
+                    <p>Improve the readability of the following code </p>
+                    <img src={assets.code_icon}/>
+                </div>
+            </div>
+
+        </div>
+
       
     </div>
   )
